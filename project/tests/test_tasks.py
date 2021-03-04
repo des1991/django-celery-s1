80deaa3bdec493fc82237d366d27a80ddfbe9d46
Denis Pak <des1991@gmail.com>
2021-03-04 16:43:48 +0300

    Django + Celery series 1

--- project/tests/test_tasks.py
+++ project/tests/test_tasks.py
@@ -1,7 +1,14 @@
 from django.urls import reverse
 
+from tasks import sample_tasks
+
 
 def test_home(client):
     url = reverse("home")
     response = client.get(url)
     assert response.status_code == 200
+
+def test_task():
+    assert sample_tasks.create_task.run(1)
+    assert sample_tasks.create_task.run(2)
+    assert sample_tasks.create_task.run(3)
